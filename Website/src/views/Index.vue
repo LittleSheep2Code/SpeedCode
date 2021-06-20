25733ce8bfb387e54cde2b58f4980f8c258bf6c6
LittleSheep_ <cadenjiang@outlook.com>
2021-06-20 22:38:24 +0800

    Completed website app.vue and basic layout

---
+++ Website/src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-main>
+
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Website/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'en-US',
+      fallbackLocale: 'en-US',
+      localeDir: 'i18n',
+      enableInSFC: false
+    }
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
